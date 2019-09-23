088d252548573d28330f1a0c1dfc738d984a5275
Michel <Michel@mzaal.nl>
2019-09-23 12:23:41 +0200

    First Init

---
+++ Area52/Modules/Nofity/nottiffy2.2.py
@@ -0,0 +1,6 @@
+import notify2
+help(notify2)
+# notify2.init('msg')
+# n = notify2.Notification("Summary", "Some Body Text") #Icon Name
+# # n = notify2.Notification("Summary","Some Body Text", "notification-message-im") #Icon Name
+# n.show()
